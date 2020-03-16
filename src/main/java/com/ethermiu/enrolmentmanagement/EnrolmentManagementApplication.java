72aeae625cafa9ac34d291f2d70333ce122361ed
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-03-16 14:23:40 -0500

    Initial commit

---
+++ src/main/java/com/ethermiu/enrolmentmanagement/EnrolmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ethermiu.enrolmentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolmentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolmentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ethermiu/enrolmentmanagement/EnrolmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethermiu.enrolmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrolmentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
