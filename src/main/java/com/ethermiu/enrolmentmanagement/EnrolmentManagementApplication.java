a1416fb5446e954c5bda4cf18d0d55877a4fcc44
Resty Nasimbwa <restynan@gmail.com>
2020-03-16 20:31:30 -0500

    test

--- src/main/java/com/ethermiu/enrolmentmanagement/EnrolmentManagementApplication.java
+++ src/main/java/com/ethermiu/enrolmentmanagement/EnrolmentManagementApplication.java
@@ -8,6 +8,7 @@ public class EnrolmentManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EnrolmentManagementApplication.class, args);
+        System.out.println("Testing ");
     }
 
 }
