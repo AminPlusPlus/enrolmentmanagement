381317c3f34c3b38526b2e28bd539814c1ee505e
yowee <yomefisseha43@gmail.com>
2020-03-17 22:04:51 -0500

    Student Service and persitance Layer

---
+++ src/main/java/com/ethermiu/enrolmentmanagement/service/impl/StudentServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ethermiu.enrolmentmanagement.service.impl;
+
+import com.ethermiu.enrolmentmanagement.domain.Student;
+import com.ethermiu.enrolmentmanagement.repository.StudentRepository;
+import com.ethermiu.enrolmentmanagement.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Override
+    public void create(Student student) {
+        studentRepository.save(student);
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return studentRepository.findById(id).get();
+    }
+
+    @Override
+    public List<Student> getAllStudent() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public void update(Long id,Student student) {
+       Student student1 = studentRepository.getOne(id);
+       if (student1==null){
+           throw  new IllegalArgumentException("Not Able to Find student With this Id");
+       }
+       else {
+           student1=student;
+           studentRepository.save(student1);
+       }
+    }
+
+    @Override
+    public void delete(Student student) {
+        studentRepository.delete(student);
+    }
+    public void deleteAll() {
+        studentRepository.deleteAll();
+    }
+}
