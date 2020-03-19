a335c42ac30881ca66c9106e1291616629690f34
yowee <yomefisseha43@gmail.com>
2020-03-19 11:31:30 -0500

    Section updated.

--- src/main/java/com/ethermiu/enrolmentmanagement/domain/Section.java
+++ src/main/java/com/ethermiu/enrolmentmanagement/domain/Section.java
@@ -1,5 +1,6 @@
 package com.ethermiu.enrolmentmanagement.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,19 +15,24 @@ import java.util.List;
 public class Section {
     @Id
     @GeneratedValue
-    private int id;
-    @ManyToOne
+    private Long id;
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.PERSIST)
     @JoinColumn(name = "faculty_id")
     private Faculty faculty;
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "offering_id")
     private Offering offering;
 
-    @ManyToMany
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
     @JoinTable(name = "Enrollment",
             joinColumns = { @JoinColumn(name = "section_id") },
             inverseJoinColumns = { @JoinColumn(name = "student_id") } )
-    List<Student> studentList = new ArrayList<>();
+    List<Student> studentList = new ArrayList<Student>();
 
+    public  void addStudent(Student student){
+        studentList.add(student);
+    }
 
 }
