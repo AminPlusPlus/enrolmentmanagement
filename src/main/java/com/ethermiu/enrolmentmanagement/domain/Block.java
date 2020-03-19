ab56eb275d6a478c4b6cf906c12767c339082545
Adnan Shehzad <adnanshahzad00003@gmail.com>
2020-03-18 21:45:18 -0500

    Block completed

--- src/main/java/com/ethermiu/enrolmentmanagement/domain/Block.java
+++ src/main/java/com/ethermiu/enrolmentmanagement/domain/Block.java
@@ -24,8 +24,6 @@ public class Block {
     private int blockSequenceNumber;
     private Date startDate;
     private Date endDate;
-//    @OneToMany(mappedBy = "block")
-//    List<Offering> offeringList;
     }
 
 
