5d233302c7f45f71eb847703140d1a7fd1f2379a
Aminjoni Abdullozoda <amin030296@gmail.com>
2020-03-19 13:54:26 -0500

    Swagger fixed!

--- src/main/java/com/ethermiu/enrolmentmanagement/config/SecurityConfiguration.java
+++ src/main/java/com/ethermiu/enrolmentmanagement/config/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -73,9 +74,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .antMatchers("/swagger-ui.html#/").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/api/v1/courses").hasAnyRole("STUDENT","ADMIN")
                 .antMatchers("/api/v1/offers").hasAnyRole("ADMIN","STUDENT")
                 .antMatchers("api/v1/blocks").hasAnyRole("ADMIN","STUDENT","FACULTY")
